972b7e8de7f157fc502ff3983e562580a3a00a58
陈龙 <45967413@qq.com>
2020-08-25 11:48:25 +0800

    首页提交

--- js/index.js
+++ js/index.js
@@ -2,29 +2,26 @@ jQuery(function () {
 	function initBox() {
 		w_width = jQuery(window).width();
 		w_height = jQuery(window).height();
-		$('.banner-swiper').css('height', w_height);
-		$('.banner-item,.banner-item figure').css('height', w_height);
-		setImgMax($('.banner-item figure img'), 1920, 1080, w_width, w_height);
+		let mtop = jQuery('.mtop').height();
+		$('.banner-swiper').css('height', w_height-mtop);
+		$('.banner-item,.banner-item figure').css('height', w_height-mtop);
+		setImgMax($('.banner-item figure img'), 1920, 814, w_width, w_height-mtop);
 	};
 	initBox();
 	jQuery(window).resize(function () {
 		initBox();
 	});
 
-	new SplitText($('.banner-text strong'), {type: "chars"});
-	new SplitText($('.banner-text small'), {type: "chars"});
-	new SplitText($('.banner-text p'), {type: "chars"});
-	
-	$('.banner-swiper').on('init', function (event, slick, currentSlide, nextSlide) {
-		$(".banner-item").eq(0).addClass("aimation").siblings().removeClass("aimation");
-		$('.banner-line i').css({width:0}).stop().animate({width:100+'%'},5000,'linear');
+	$('.banner').on('init', function (event, slick, currentSlide, nextSlide) {
 		$(".banner-item").first().removeClass("slick-current");
 		setTimeout(function(){
 			$(".banner-item").first().addClass("slick-current");
 		},50);
+		TweenLite.to($(".banner-item").eq(0).find('figure img'),0,{scale:1.15})
+		TweenMax.to($(".banner-item").eq(0).find('figure img'),5.4,{scale:1,ease:Power0.easeNone});
 	});
 
-	$(".banner-swiper").slick({
+	$(".banner").slick({
 		centerPadding: '0',
 		autoplaySpeed: 5000,
 		speed: 1800,
@@ -37,15 +34,9 @@ jQuery(function () {
 		focusOnSelect: true,
 		prevArrow: $('.banner-btns span.prev'),
 		nextArrow: $('.banner-btns span.next'),
-		responsive: [{
-			breakpoint: 1024,
-			settings: {
-				dots: true,
-			}
-		}, ]
 	}).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-		$('.banner-line i').css({width:0}).stop().animate({width:100+'%'},5000,'linear');
-		$(".banner-item").eq(nextSlide).addClass("aimation").siblings().removeClass("aimation");
+		TweenLite.to($(".banner-item").eq(nextSlide).find('figure img'),0,{scale:1.15})
+	    TweenMax.to($(".banner-item").eq(nextSlide).find('figure img'),5.4,{scale:1,ease:Power0.easeNone})
 	});
 
 });
